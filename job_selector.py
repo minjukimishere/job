39f4578afeec4e6293ea4e1a5348fb0f94223b1b
MJ <112530099+minjukimishere@users.noreply.github.com>
2024-06-18 15:47:27 +0900

    Add files via upload

--- job_selector.py
+++ job_selector.py
@@ -1,17 +1,37 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.header("AI HR", divider='orange')
+st.set_page_config(
+    page_title="노세老世",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+)
 
-col1,col2= st.columns(2)
+st.header("노세老世", divider='orange')
+st.subheader("AI HR")
+st.markdown(" ")
+
+col1,col2,col3= st.columns(3)
 with col1:
-    st.page_link("pages/match_input.py", label="고용인") 
-    st.text("AI 기반 고용 시스템을 사용 해 보시겠어요?")
+    with st.container(border=True):
+        st.subheader("구직자") 
+        st.image("images/resume.png")
+        st.markdown("AI 기반 이력서를 작성 해 드려요.")
+        st.markdown("---")
+        st.page_link("pages/resume_make.py", label="이력서 생성하기") 
+        
 with col2:
-    st.page_link("pages/resume_make.py", label="구직자") 
-    st.text("AI 기반 이력서를 작성 해 드려요.")
+    with st.container(border=True):
+        st.subheader("고용인") 
+        st.image("images/system.png")
+        st.markdown("AI 기반 고용 시스템을 사용 해 보시겠어요?")
+        st.markdown("---")
+        st.page_link("pages/match_input.py", label="인재 추천받기") 
 
-st.markdown(' ')
-
-st.page_link("pages/ai_interview.py", label="인터뷰") 
-st.text("AI 모의 인터뷰")
+with col3:
+    with st.container(border=True):
+        st.subheader("인터뷰")
+        st.image("images/interview.png")
+        st.markdown("AI와 모의 면접을 진행합니다.")
+        st.markdown("---")
+        st.page_link("pages/ai_interview.py", label="모의면접 하러가기") 
--- pages/match_input.py
+++ pages/match_input.py
@@ -1,57 +1,43 @@
 import streamlit as st
 
-# 제목과 설명 추가
-st.title('채용하고 싶은 인재 조건이 있으신가요?')
-st.markdown("")
+st.set_page_config(
+    page_title="노세老世",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+)
 
-st.markdown("**직무를 선택해주세요**") # 별 두 개는 bold
+# Streamlit 애플리케이션 구성
+st.header("노세老世 | AI HR", divider='orange')
+st.subheader("인재추천")
+st.markdown(" ")
 
-# Jobs 입력 받기 (여러 개 선택 가능)
-jobs = st.multiselect(
-    "직무를 선택해주세요.",
-    ["DevOps/시스템 관리자", "HW/임베디드 개발자", "IOS 개발자",
-     "데이터 사이언티스트", "데이터 엔지니어", "서버/백엔드 개발자",
-     "안드로이드 개발자", "프론트엔드 개발자", "게임 서버 개발자",
-     "게임 클라이언트 개발자"])
+# 직무 선택 옵션 정의
+jobs_options = ["DevOps/시스템 관리자", "HW/임베디드 개발자", "IOS 개발자",
+                "데이터 사이언티스트", "데이터 엔지니어", "서버/백엔드 개발자",
+                "안드로이드 개발자", "프론트엔드 개발자", "게임 서버 개발자",
+                "게임 클라이언트 개발자"]
 
-st.write("선택한 직무:", jobs)
+# UI 레이아웃 설정
+col1, col2 = st.columns([2, 2])
 
-# start_year 입력 받기, end_year 입력 받기
-start_year, end_year = st.select_slider(
-    "연차를 선택해주세요",
-    options=["신입", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15년 이상"],
-    value=("신입", "15년 이상"))
+with col1:
+    jobs = st.multiselect(
+        "채용하고 싶은 직무를 입력 해 주세요.",
+        jobs_options
+    )
 
-st.write("입사 연차:", start_year)
-st.write("퇴사 연차:", end_year)
+with col2:
+    start_year, end_year = st.select_slider(
+        "채용하고 싶은 연차를 선택해주세요",
+        options=["신입", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15년 이상"],
+        value=("신입", "15년 이상")
+    )
 
-# URL 입력 받기
-url = st.text_input("채용공고 URL을 입력해주세요")
-
-st.write("URL:", url)
-
-# 상태 저장
-if 'jobs' not in st.session_state:
-    st.session_state.jobs = []
-if 'start_year' not in st.session_state:
-    st.session_state.start_year = ""
-if 'end_year' not in st.session_state:
-    st.session_state.end_year = ""
-if 'url' not in st.session_state:
-    st.session_state.url = ""
-
+# 세션 상태에 입력 데이터 저장
 st.session_state.jobs = jobs
 st.session_state.start_year = start_year
 st.session_state.end_year = end_year
-st.session_state.url = url
-
+st.markdown("---")
 # 페이지 이동 버튼
 st.page_link("pages/match_output.py", label="인재 추천 받기")
 
-# 확인용 결과창
-if st.button('결과 출력'):
-    st.markdown("### 결과:")
-    st.write(f"jobs: {st.session_state.jobs}")
-    st.write(f"start_year: {st.session_state.start_year}")
-    st.write(f"end_year: {st.session_state.end_year}")
-    st.write(f"url: {st.session_state.url}")
